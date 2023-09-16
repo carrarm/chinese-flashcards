f4b9486e9d4a5ca539df410621c5029ce15414b5
Carrm <marinecr_dev@fastmail.com>
2023-09-16 07:59:48 +0200

    feat(session filling results): show the diff with the solution

--- src/app/pages/learn/session/session-filling-step/result-card/result-card.component.ts
+++ src/app/pages/learn/session/session-filling-step/result-card/result-card.component.ts
@@ -15,13 +15,50 @@ export class ResultCardComponent implements OnInit {
   @Input() expected?: TypedContent;
   @Input({ required: true }) result!: "failed" | "passed" | "solution";
 
+  public diff?: { characters: DiffCharacter[]; pinyin: DiffCharacter[] };
   public resultIcon: IconDefinition = faCheckCircle;
 
   ngOnInit(): void {
     if (this.result === "failed") {
       this.resultIcon = faXmarkCircle;
+      if (this.expected) {
+        this.diff = {
+          characters: this.computeDiff(this.expected.characters, this.content.characters),
+          pinyin: this.computeDiff(this.expected.pinyin, this.content.pinyin),
+        };
+      }
     }
   }
+
+  /**
+   * Computes the diff char by char between the expected and typed characters
+   * with `typed` as the reference for the diff result.
+   *
+   * @param expected Expected characters
+   * @param typed Characters to compare
+   * @returns The `success` or `error` status for each `typed`'s character
+   */
+  private computeDiff(expected?: string, typed?: string): DiffCharacter[] {
+    const diff: DiffCharacter[] = [];
+    if (expected && typed) {
+      typed.split("").forEach((char, index) => {
+        if (index < expected.length) {
+          diff.push({
+            char,
+            type: char === expected.charAt(index) ? "success" : "error",
+          });
+        } else {
+          diff.push({ char, type: "error" });
+        }
+      });
+    }
+    return diff;
+  }
+}
+
+interface DiffCharacter {
+  char: string;
+  type: "success" | "error";
 }
 
 interface TypedContent {
