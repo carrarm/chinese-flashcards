4da907d5e82c8bb3be029e7482a4bb2df93d7749
carrarm <carrmdev@fastmail.com>
2025-08-11 09:55:36 +0200

    chore: update Angular to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule, SwUpdate } from "@angular/service-worker";
 
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { checkForUpdates } from "@core/pwa-updates";
 import { NgCircleProgressModule } from "ng-circle-progress";
@@ -15,34 +15,28 @@ import { CollectionPageModule } from "./pages/collection/collection-page.module"
 import { LearnPageModule } from "./pages/learn/learn-page.module";
 import { SettingsModule } from "./pages/settings/settings.module";
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    CollectionPageModule,
-    HttpClientModule,
-    LearnPageModule,
-    NavbarComponent,
-    NgCircleProgressModule.forRoot(),
-    ServiceWorkerModule.register("ngsw-worker.js", {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: "registerWhenStable:30000",
-    }),
-    SettingsModule,
-    TabBarComponent,
-  ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: checkForUpdates,
-      multi: true,
-      deps: [SwUpdate, MatSnackBar],
-    },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        CollectionPageModule,
+        LearnPageModule,
+        NavbarComponent,
+        NgCircleProgressModule.forRoot(),
+        ServiceWorkerModule.register("ngsw-worker.js", {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: "registerWhenStable:30000",
+        }),
+        SettingsModule,
+        TabBarComponent], providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: checkForUpdates,
+            multi: true,
+            deps: [SwUpdate, MatSnackBar],
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
